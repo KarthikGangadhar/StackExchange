4d1160ee15e9b02a9005de8a1411f4e385943ee1
Karthik <kgangadhar@corp.homestore.net>
2017-12-11 12:55:44 +0530

    initial commit

---
+++ lib/stack/exchange.rb
@@ -0,0 +1,7 @@
+require "stack/exchange/version"
+
+module Stack
+  module Exchange
+    # Your code goes here...
+  end
+end
---
+++ lib/stack/exchange/version.rb
@@ -0,0 +1,5 @@
+module Stack
+  module Exchange
+    VERSION = "0.1.0"
+  end
+end
